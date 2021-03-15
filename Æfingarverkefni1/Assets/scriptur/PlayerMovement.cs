1303030d28a31942a7f2b59a84e2fcbb796dd4d2
larusarmann <larus.armann@gmail.com>
2021-03-15 12:06:11 +0000

    æfingarverkefni

---
+++ Æfingarverkefni1/Assets/scriptur/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody leikmadur;
+    public float hradi = 20;
+    public float hlidarhradi = 20;
+    void Start()
+    {
+        debug.Log("Halló Lárus hér");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * hradi * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += -transform.forward * hradi * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * hlidarhradi * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += -transform.right * hlidarhradi * Time.deltaTime;
+        }
+    }
+}
