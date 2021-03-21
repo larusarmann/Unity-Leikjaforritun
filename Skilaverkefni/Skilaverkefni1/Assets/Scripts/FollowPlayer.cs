03706adae8726d4dce12514fb9e6c9c24cd5c3a5
larusarmann <larus.armann@gmail.com>
2021-03-21 23:55:07 +0000

    Skilaverkefni 1 Klárað

---
+++ Skilaverkefni/Skilaverkefni1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿// Lárus Ármann Kjartansson - 19/mars/2021
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour// býr til klasa
+{
+    public GameObject player; //býr til playerinn
+    private Vector3 viewOffset = new Vector3(0, 8, -12);// segir hve langt á bakvið myndavélin eigi að vera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + viewOffset;// uppfærir það í hverju "frame"
+    }
+}
---
+++ Skilaverkefni/Skilaverkefni1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿// Lárus Ármann Kjartansson - 19/mars/2021
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour// býr til klasa
+{
+    private float speed = 5.0f; //setur hraðan
+    private float turnSpeed = 25.0f; // segjir hversu hratt á að snúa bílnum þegar er beygt
+    private float horizontalInput; // stjórnar vinstri og hægri örvartökkum
+    private float forwardInput; // stjórnar áfram og aftur örvartökkum
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");// skrifar inn í horizontalInput
+        forwardInput = Input.GetAxis("Vertical");// skrifar inn í forwardInput
+
+        // Move the vehicle based on input params
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);// keyrir áfram
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput); // leyfir bílnum að beygja á meðan er keyrt áfram, annars hefði hann bara farið beint til hliðar og bíllin ekkert snúist í leiðinni
+    }
+}
