2904a52da49b81c78a6811d82f578c7dc2fe9909
larusarmann <larus.armann@gmail.com>
2021-03-23 15:10:53 +0000

    n

---
+++ Skilaverkefni/Skilaverkefni2/Assets/scriptur/Lokastig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lokastig : MonoBehaviour
+{
+    public Text stig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        stig.text = "Stig: " + PlayerMovment.count.ToString();
+    }
+
+    
+}
--- Skilaverkefni/Skilaverkefni2/Assets/scriptur/playerFollower.cs
+++ Skilaverkefni/Skilaverkefni2/Assets/scriptur/playerFollower.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class playerFollower : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform player;
     public Vector3 offset;
+ 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position+offset;
-        transform.rotation = player.rotation;
+        transform.position = player.position + offset;
+        //transform.rotation = player.rotation ;
+
     }
+  
 }
+
