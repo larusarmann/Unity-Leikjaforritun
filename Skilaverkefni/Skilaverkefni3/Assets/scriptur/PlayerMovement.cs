f5a85a2ef870808b29d720ea2c8559c643ebeb14
larusarmann <larus.armann@gmail.com>
2021-04-13 14:49:44 +0000

    smá push

---
+++ Skilaverkefni/Skilaverkefni3/Assets/scriptur/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed=100f;
+    public float sideways = 20f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+           
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * sideways * Time.deltaTime;
+          
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += -transform.right * sideways * Time.deltaTime;
+          
+        }
+        if (Input.GetKey("a"))
+        {
+            Vector3 snu = new Vector3(0, 2, 0);
+            transform.Rotate(snu, Space.World);
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector3 snu = new Vector3(0, -2, 0);
+            transform.Rotate(snu, Space.World);
+        }
+    }
+}
---
+++ Skilaverkefni/Skilaverkefni3/Assets/scriptur/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+    }
+
+  
+}
